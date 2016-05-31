5458111755d34741a289bb67ffbbf44fa0a36c43
GabrielPontin <gabrielpontin01@gmail.com>
2016-05-31 20:21:25 -0300

    atualizaçao no Meu Data Source

---
+++ README.md
@@ -0,0 +1,14 @@
+# ReportSystem
+
+Este projeto foi desenvolvido mediante exercício proposto em sala pelo professor da disciplina de Programação Orientada a Objetos.
+O mesmo é responsável por gerar e disponibilizar dados para impressão de relatórios.
+
+Foram usadas as seguintes tecnologias:
+
+- Java
+- Jasper
+
+Funcionalidades para próximas versões:
+
+Adicionar executavel para gerar relatórios;
+Realizar conexão com banco de dados sql e/ou nosql;
--- src/br/univel/MeuDataSource.java
+++ src/br/univel/MeuDataSource.java
@@ -1,21 +1,24 @@
 package br.univel;
 
+import net.sf.jasperreports.engine.JRDataSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JRField;
 
-public class MeuDataSource implements JRDataDource {
-	
+public class MeuDataSource implements JRDataSource {
+
 	private int contador = 0;
-	
+
 	@Override
-	public Object getFieldValue(JRField arg0) throws JRException{
+	public Object getFieldValue(JRField arg0) throws JRException {
 		if(arg0.getName().equals("id")){
-			return String.valueOf("contador");
+			return String.valueOf(contador);
 		}
-		return "Mah Oee!";
+		return null;
 	}
+
 	@Override
-	public boolean next() throws JRException{
+	public boolean next() throws JRException {
 		return contador++ < 10;
 	}
+
 }
